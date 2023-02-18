60acd1c8592419013b1dc92538c73189b31efebf
Moustafa_Khedr <mustfakhedr1998@gmail.com>
2023-02-18 16:36:04 +0300

    add paginations for project

--- product/views.py
+++ product/views.py
@@ -1,11 +1,23 @@
 from django.shortcuts import render
 from .models import Product
-
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 # Create your views here.
 
 def product_list(request):
     product_list = Product.objects.all()
-    return render(request, 'Product/product_list.html',{'product_list':product_list})
+    paginator = Paginator(product_list, 2)
+    page_number = request.GET.get('page')
+    try:
+        page_obj= paginator.get_page(page_number)
+    except PageNotAnInteger:
+        page_obj = paginator.page(1)
+    except EmptyPage:  
+        page_obj = paginator.page(paginator.num_pages)
+        
+    return render(request, 'Product/product_list.html',
+                  {'product_list':page_obj,
+                   }
+                  )
 
 
 def product_detail(request, slug):
